be3b295c11eecbeb445b45844ec02fc73ea2b5df
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-13 20:41:15 +1000

    Updated egisz.c

--- egisz.c
+++ egisz.c
@@ -54,8 +54,8 @@ int main(int argc, char *argv[])
 	//cJSON *json = egisz_depart_part(1, 10);
 	//cJSON *json = egisz_org_part(2, 1);
 	//cJSON *json = egisz_org_get("1.2.643.5.1.13.13.12.1.42.202");		
-	cJSON *json = egisz_depart_get("1.2.643.5.1.13.13.12.2.51.4977.0.2787.2107");		
-	//cJSON *json = egisz_mo("1.2.643.5.1.13.13.12.1.42.202");		
+	//cJSON *json = egisz_depart_get("1.2.643.5.1.13.13.12.2.51.4977.0.2787.2107");		
+	cJSON *json = egisz_mo("1.2.643.5.1.13.13.12.1.42.202");		
 	//cJSON *json = egisz_rest_lic_get(1);		
 	print_json(json);
 	//printf("ITEM: %s\n", cJSON_Print(json));	
