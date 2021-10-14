5f25f6e1633b5c266145496dc31bf42736c0cccd
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-14 15:13:05 +1000

    Updated soap_test.c

--- soap_test.c
+++ soap_test.c
@@ -6,6 +6,7 @@
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
  */
 #include "soap_test.h"
+#include <egisz_ssl_connector.h>
 
 #define HOST " https://nsi.rosminzdrav.ru/wsdl/SOAP-server.v2.php"
 #define PORT 443
