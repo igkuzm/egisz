5932300280ba924c541721bc080ed958a051c4e5
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-14 15:11:20 +1000

    Updated soap_test.h

---
+++ soap_test.h
@@ -0,0 +1,10 @@
+/**
+ * File              : soap_test.h
+ * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
+ * Date              : 14.10.2021
+ * Last Modified Date: 14.10.2021
+ * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
+ */
+#include <stdio.h>
+
+int soap_test();
