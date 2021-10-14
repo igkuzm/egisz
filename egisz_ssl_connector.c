a0e0b118ccf9bafb90fe9493886add059d3a4fe4
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-14 11:35:04 +1000

    Updated egisz_ssl_connector.c

--- egisz_ssl_connector.c
+++ egisz_ssl_connector.c
@@ -119,7 +119,9 @@ int egisz_ssl_connector_socket(const char *hostname, int port, const char *apike
 		return -1;
 	} 
     int sd = socket(PF_INET, SOCK_STREAM, 0); //init socket
-    bzero(&addr, sizeof(addr));
+    //bzero(&addr, sizeof(addr));
+	memset(&addr, 0, sizeof(addr));
+
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
     addr.sin_addr.s_addr = *(long*)(host->h_addr);
