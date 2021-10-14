94d4972595b2def4f6eb41fa4b596db3b3bdddb9
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-14 15:11:44 +1000

    Updated soap_test.c

---
+++ soap_test.c
@@ -0,0 +1,13 @@
+/**
+ * File              : soap_test.c
+ * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
+ * Date              : 14.10.2021
+ * Last Modified Date: 14.10.2021
+ * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
+ */
+#include "soap_test.h"
+
+int soap_test(){
+
+	return 0;
+}
