990e3a9c742546aad6f1b5472f734fc4bd94f7ee
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-15 12:51:15 +1000

    Updated http_connector.h

--- http_connector.h
+++ http_connector.h
@@ -5,5 +5,17 @@
  * Last Modified Date: 15.10.2021
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
  */
+#include <stdio.h>
+
+typedef enum {
+	HTTP,
+	HTTPS
+} Http_protocol;
+
+typedef struct {
+	Http_protocol protocol;
+	char hostname[256];
+	char request[BUFSIZ];
+} Http_method;
 
 int parse_http_string(const char *http_string, Http_method *method);
