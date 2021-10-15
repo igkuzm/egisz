1cf24fdcfa1af58081931bdaa7c880bdf8c0fb3b
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-15 14:12:58 +1000

    Updated http_connector.c

--- http_connector.c
+++ http_connector.c
@@ -46,13 +46,7 @@ int parse_http_string(const char *http_string, HTTP_GET *http_get){
 
 		if (protocol_is_set && !hostname_is_set) {
 			if (http_string[i] == '/' || http_string[i + 1] == '\0') {
-				if (http_string[i + 1] == '\0') {
-					buf[buf_len + 1] = http_string[i + 1];
-					strncpy(hostname_string, buf, buf_len + 1);				
-				}
-				else {
-					strncpy(hostname_string, buf, buf_len);				
-				}
+				strncpy(hostname_string, buf, buf_len);				
 				memset(buf, 0, buf_len);
 				buf_len = 0;
 				hostname_is_set = true;
@@ -61,7 +55,6 @@ int parse_http_string(const char *http_string, HTTP_GET *http_get){
 		}
 		if (hostname_is_set) {
 			if (http_string[i + 1] == '\0') {
-				buf[buf_len] = http_string[i];
 				strncpy(request_string, buf, buf_len);				
 			}			
 		}		
