e64b0bac9ccc4435a5749ece7cc60b50eabb0ee3
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-15 11:34:46 +1000

    Updated soap_test.c

--- soap_test.c
+++ soap_test.c
@@ -2,7 +2,7 @@
  * File              : soap_test.c
  * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
  * Date              : 14.10.2021
- * Last Modified Date: 14.10.2021
+ * Last Modified Date: 15.10.2021
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
  */
 #include "soap_test.h"
@@ -74,7 +74,7 @@ int soap_test(){
 	}  
 
 	//generage HTTP REQUEST MESSAGE
-	char *write_buf = "PUT ";
+	char *write_buf = "PUT";
 	//SSL WRITE
 	int retval;
 	retval = SSL_write(ssl, write_buf, strlen(write_buf));
