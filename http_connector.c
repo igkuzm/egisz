511250547f1afadda8ab9ca7523d2d47d9c92810
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-15 14:00:58 +1000

    Updated http_connector.c

--- http_connector.c
+++ http_connector.c
@@ -140,8 +140,8 @@ int http_connector(const char *http_string, void *data, int (*callback)(char*,in
 	//generage HTTP REQUEST MESSAGE
 	char write_buf[2*BUFSIZ];
 	sprintf(write_buf, "GET");
-	if (snprintf(write_buf, BUFSIZ, "%s %s", write_buf, http_get.request) == -1){
-		fprintf(stderr, "Error. Can't merge http request for host: %s with http request: GET %s\n", http_get.hostname, http_get.request);	
+	if (snprintf(write_buf, BUFSIZ, "%s %s HTTP/1.1\r\n", write_buf, http_get.request) == -1){
+		fprintf(stderr, "Error. Can't merge http request for host: %s with http request: GET %s HTTP/1.1\n", http_get.hostname, http_get.request);	
 		return -1;		
 	}
 
