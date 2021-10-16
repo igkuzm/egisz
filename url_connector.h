785df970c6506eaa0ce78c1b0951e1e42f39a9c0
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-17 09:23:24 +1000

    Updated url_connector.h

--- url_connector.h
+++ url_connector.h
@@ -49,7 +49,7 @@ typedef struct httpHeaderItemList{
 
 typedef struct {
 	URL_CONNECTION_PROTOCOL protocol;
-	char httpMethod[128];
+	HTTP_METHOD httpMethod;
 	char hostname[256];
 	int port;
 	char requestString[BUFSIZ];
