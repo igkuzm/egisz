3fb569e30571dae7c679cceeda407adcdd0ddfdf
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-16 23:37:12 +1000

    Updated url_connector.c

--- url_connector.c
+++ url_connector.c
@@ -90,7 +90,7 @@ URLRequest *url_request_new(){
 	request->headerItemList = headerItemList;
 	strcpy(request->httpMethod, "GET");
 
-	url_request_add_header_item(request, HTTP_HEADER_ITEM_KEY_Content_Type, "");
+	url_request_add_header_item(request, HTTP_HEADER_ITEM_KEY_Content_Type, "text/html; charset=utf-8");
 
 	return request;
 }
