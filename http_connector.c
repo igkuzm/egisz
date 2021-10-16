e0f2f7a926e68ae0f18ece12888991f1dc49bbab
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-16 22:18:46 +1000

    Updated http_connector.c

--- http_connector.c
+++ http_connector.c
@@ -32,6 +32,7 @@ const char *HTTPHeaderItemKey(HTTP_HEADER_ITEM_KEY key)
     switch (key)
     {
 		case HTTP_HEADER_ITEM_KEY_Content_Length: return "Content-Length";
+		case HTTP_HEADER_ITEM_KEY_Content_Type: return "Content-Type";
 		case HTTP_HEADER_ITEM_KEY_Authorization: return "Authorization";
 		case HTTP_HEADER_ITEM_KEY_Connection: return "Connection";
 		case HTTP_HEADER_ITEM_KEY_Host: return "Host";
