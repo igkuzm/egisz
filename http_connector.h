7890eaa4ca41f1a7bd3d837af8479ea63293c2d3
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-16 23:05:47 +1000

    Updated http_connector.h

--- http_connector.h
+++ http_connector.h
@@ -55,7 +55,7 @@ URLRequest *url_request_new_with_string(const char *url_string);
 void url_request_free(URLRequest *request);
 
 void url_request_set_http_method(URLRequest *request, const char *method);
-void url_request_add_header_item(URLRequest *request, const char *key, const char *value);
+void url_request_add_header_item(URLRequest *request, HTTP_HEADER_ITEM_KEY key, const char *value);
 
 void url_request_set_http_body_from_file(URLRequest *request, const char *filename);
 
