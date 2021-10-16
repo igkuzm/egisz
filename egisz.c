5421277d4544f60c84c0960a8f83f66cf2608e6f
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-16 23:19:52 +1000

    Updated egisz.c

--- egisz.c
+++ egisz.c
@@ -2,14 +2,14 @@
  * File              : egisz.c
  * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
  * Date              : 11.10.2021
- * Last Modified Date: 15.10.2021
+ * Last Modified Date: 16.10.2021
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
  */
 
 #include "egisz.h"
 #include "cJSON.h"
 //#include "rest_service_NR.h"
-#include "http_connector.h"
+#include "url_connector.h"
 #include <stdio.h>
 #include <stdlib.h>
 void print_json(cJSON *json){
