1f2fa1d255d9a5657f81276c1256a55767ec5a74
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-16 21:22:08 +1000

    Updated http_connector.h

--- http_connector.h
+++ http_connector.h
@@ -33,8 +33,8 @@ typedef struct{
 } HTTPHeaderItem;
 
 typedef struct httpHeaderItemList{
-	HTTPHeaderItemList *prev;
-	HTTPHeaderItemList *next;
+	httpHeaderItemList *prev;
+	httpHeaderItemList *next;
 } HTTPHeaderItemList;
 
 typedef struct {
