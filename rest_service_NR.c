74ddb2eed3dc3f8b68644e69af7f9c23a493f30d
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-17 21:39:07 +1000

    Updated rest_service_NR.c

--- rest_service_NR.c
+++ rest_service_NR.c
@@ -56,6 +56,8 @@ cJSON *json_from_egisz_ssl_connector_answer(char *method, int argc, char *argv[]
 	return json;
 }
 URLRequest *url_request_prepare(){
+	URLRequest *request = url_request_new();
+	url_request_set_http_method(request, HTTP_METHOD_GET);
 
 }
 
