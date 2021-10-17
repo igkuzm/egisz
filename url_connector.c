be35810613e3db08d1fb2bb4a02c037ef0a32d67
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-17 21:31:50 +1000

    Updated url_connector.c

--- url_connector.c
+++ url_connector.c
@@ -75,7 +75,6 @@ void url_request_add_header_item(URLRequest *request, HTTP_HEADER_ITEM_KEY key, 
 		
 		HTTPHeaderItemList *newHeaderItemList = malloc(sizeof(HTTPHeaderItemList));
 		newHeaderItemList->prev = headerItemList;
-		newHeaderItemList->next = NULL;
 		newHeaderItemList->headerItem = item;
 
 		request->headerItemList = newHeaderItemList;
@@ -97,7 +96,7 @@ URLRequest *url_request_new(){
 		return NULL;
 	}
 	
-	headerItemList->prev = headerItemList->next = NULL;
+	headerItemList->prev = NULL;
 	request->headerItemList = headerItemList;
 	request->method = HTTP_METHOD_GET;
 
