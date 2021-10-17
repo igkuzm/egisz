e821056e43e396669c2a99052d4f4e1f1cd3ceb6
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-17 22:54:41 +1000

    Updated url_connector.c

--- url_connector.c
+++ url_connector.c
@@ -383,11 +383,11 @@ int url_connection_send_request_ssl(int sd, char *write_buf, void *data, int (*c
 		}
 	}
 	printf("OKOKOKOKO\n");
-	if (bytes < 0 ){ //hendle with error
-		_handle_with_ssl_error(ssl, bytes);
-		fprintf(stderr, "Error while SSL_read\n");
-		return bytes;			
-	}	
+	//if (bytes < 0 ){ //hendle with error
+		//_handle_with_ssl_error(ssl, bytes);
+		//fprintf(stderr, "Error while SSL_read\n");
+		//return bytes;			
+	//}	
 
 	//Close SSL
 	SSL_free(ssl);   
