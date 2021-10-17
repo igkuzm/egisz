d2894ee54b91269a5ac7ffb7d27703fa8ee5565a
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-17 21:50:20 +1000

    Updated url_connector.h

--- url_connector.h
+++ url_connector.h
@@ -63,6 +63,8 @@ void url_request_free(URLRequest *request);
 
 void url_request_set_url_connection_protocol(URLRequest *request, URL_CONNECTION_PROTOCOL protocol);
 void url_request_set_http_method(URLRequest *request, HTTP_METHOD method);
+void url_request_set_hostname(URLRequest *request, char *hostname);
+void url_request_set_port(URLRequest *request, int port);
 void url_request_add_header_item(URLRequest *request, HTTP_HEADER_ITEM_KEY key, const char *value);
 
 void url_request_set_http_body_from_string(URLRequest *request, const char *contentType, char *bodyString);
