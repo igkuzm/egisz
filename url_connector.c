56b14280ecb530ebff5a39cfad6684bd1bd3031a
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-17 23:14:54 +1000

    Updated url_connector.c

--- url_connector.c
+++ url_connector.c
@@ -438,18 +438,18 @@ int url_connection_send_request(URLRequest *request, void *data, int (*callback)
 	
 	//Send request
 
-	//if (request->protocol == URL_CONNECTION_PROTOCOL_HTTPS) {
+	if (request->protocol == URL_CONNECTION_PROTOCOL_HTTPS) {
 		url_connection_send_request_ssl(sd, write_buf, data, callback);
-	//}	
+	}	
 
-	//if (request->protocol == URL_CONNECTION_PROTOCOL_HTTP) {
-		//if (url_connection_send_request_no_ssl(sd, write_buf, data, callback)){
+	if (request->protocol == URL_CONNECTION_PROTOCOL_HTTP) {
+		if (url_connection_send_request_no_ssl(sd, write_buf, data, callback)){
 
-		//}
-	//}
+		}
+	}
 
-	//free(write_buf);
-	//close(sd);      
+	free(write_buf);
+	close(sd);      
 
 	return 0;
 }
