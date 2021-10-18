09ee9c3221f3168d0f45be61fcc6b5ee770d7064
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 18:26:18 +1000

    Updated list.c

---
+++ list.c
@@ -0,0 +1,8 @@
+/**
+ * File              : list.c
+ * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
+ * Date              : 18.10.2021
+ * Last Modified Date: 18.10.2021
+ * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
+ */
+#include "list.h"
