dbd2e22acd67a7d53928b089fd5a4d349938a46f
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 13:21:13 +1000

    Updated rest_service_NR.c

--- rest_service_NR.c
+++ rest_service_NR.c
@@ -52,8 +52,8 @@ URLRequest *url_request_prepare(){
 
 
 cJSON *json_from_url_connection_send_request(URLRequest *request){
-	RestServiceAnswer *answer = rest_service_answer_new();
-	if (url_connection_send_request(request, answer, rest_service_answer_callback)){
+	URLConnectAnswer *answer = url_connect_answer_new();
+	if (url_connection_send_request(request, answer, url_connect_answer_callback)){
 		fprintf(stderr, "Error in function url_connection_send_request\n");
 		return NULL;
 	}
