cf794180e9fc06f82e268f88175003a2963f3cb7
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 13:29:22 +1000

    Updated url_connector.h

--- url_connector.h
+++ url_connector.h
@@ -62,7 +62,7 @@ typedef struct {
 } URLRequest;
 
 URLRequest *url_request_new(); //create new url request
-URLRequest *url_request_new_with_string(const char *url_string); //create new url request from string - eg. http://example.com/?username=user&password=password
+URLRequest *url_request_new_with_string(const char *url_string); //create new url GET request from string - eg. http://example.com/?username=user&password=password
 void url_request_free(URLRequest *request);
 
 void url_request_set_url_connection_protocol(URLRequest *request, URL_CONNECTION_PROTOCOL protocol);
