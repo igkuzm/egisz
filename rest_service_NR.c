bea4bbddec31c0034846cfe8a6325152a6402f50
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 11:00:27 +1000

    Updated rest_service_NR.c

--- rest_service_NR.c
+++ rest_service_NR.c
@@ -2,7 +2,7 @@
  * File              : rest_service_NR.c
  * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
  * Date              : 12.10.2021
- * Last Modified Date: 17.10.2021
+ * Last Modified Date: 18.10.2021
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
  */
 /**
@@ -187,15 +187,15 @@ URLRequest *url_request_prepare(){
 }
 
 cJSON *json_from_url_connection_send_request(URLRequest *request){
-	//RestServiceAnswer *answer = rest_service_answer_new();
-	//if (url_connection_send_request(request, answer, rest_service_answer_callback)){
-	if (url_connection_send_request(request, NULL, NULL)){
+	RestServiceAnswer *answer = rest_service_answer_new();
+	if (url_connection_send_request(request, answer, rest_service_answer_callback)){
+	//if (url_connection_send_request(request, NULL, NULL)){
 		fprintf(stderr, "Error in function url_connection_send_request\n");
 		return NULL;
 	}
-	cJSON *json;
-	//cJSON *json = cJSON_Parse(answer->body);
-	//rest_service_answer_free(answer);
+	//cJSON *json;
+	cJSON *json = cJSON_Parse(answer->body);
+	rest_service_answer_free(answer);
 	url_request_free(request);
 	return json;
 }
