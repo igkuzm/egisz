fb56e75bfb9453178f4da0aa66a57eaed6eb2486
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 15:25:24 +1000

    Updated url_connector.c

--- url_connector.c
+++ url_connector.c
@@ -562,6 +562,9 @@ int url_connect_answer_callback(char *str, int len, int *count, void *_answer){
 		if (we_have_chunked_size) {
 		//remove chunked_size string from str
 			if (chunked_size_is_zero){
+				for (i = 0; i < 1; ++i) {
+					memmove(&str[start_of_chunked],&str[start_of_chunked + 1],len - start_of_chunked);
+				}				
 			}
 			else {
 				for (i = 0; i < 8; ++i) {
