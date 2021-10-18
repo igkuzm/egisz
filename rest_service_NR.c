f9d49e1fadc2007b174f61433b85217f687b0ba8
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 21:04:10 +1000

    Updated rest_service_NR.c

--- rest_service_NR.c
+++ rest_service_NR.c
@@ -195,8 +195,8 @@ int egisz_rest_refs_part_all(struct egisz_rest_refs_part_struct **_array, int id
 		int count = egisz_rest_refs_part(&array, id, i);
 		int k;
 		for (k=0; k < count; k++) {
-			all_array[all_count] = array[k];
-			all_count++;
+			/*all_array[all_count] = array[k];*/
+			/*all_count++;*/
 		}
 	}
 	/*if (_array) {*/
