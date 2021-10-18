41b10f4b7659ee59983d2454a2b7b2a0e4fecdd4
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 17:21:05 +1000

    Updated egisz.c

--- egisz.c
+++ egisz.c
@@ -93,32 +93,32 @@ int main(int argc, char *argv[])
 	url_request_set_http_method(request, HTTP_METHOD_POST);
 	
 
+/*char *msg = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"*/
+/*"<SOAP-ENV:Envelope\r\n"*/
+	/*"xmlns:SOAP-ENV=\"http:schemas.xmlsoap.org/soap/envelope/\"\r\n"*/
+	/*"xmlns:SOAP-ENC=\"http:schemas.xmlsoap.org/soap/encoding/\"\r\n"*/
+	/*"xmlns:xsi=\"http:www.w3.org/2001/XMLSchema-instance\"\r\n"*/
+	/*"xmlns:xsd=\"http:www.w3.org/2001/XMLSchema\"\r\n"*/
+	/*"xmlns:ns1=\"urn:service\">\r\n"*/
+  /*"<SOAP-ENV:Body>\r\n"*/
+  /*"<ns1:getServerTime>\r\n"*/
+  /*"</ns1:getServerTime>\r\n"*/
+ /*"</SOAP-ENV:Body>\r\n"*/
+/*"</SOAP-ENV:Envelope>\r\n";*/
+	
 char *msg = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"
 "<SOAP-ENV:Envelope\r\n"
-    "xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\"\r\n"
-    "xmlns:SOAP-ENC=\"http://schemas.xmlsoap.org/soap/encoding/\"\r\n"
-    "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\r\n"
-    "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"\r\n"
-    "xmlns:ns1=\"urn:service\">\r\n"
-  "<SOAP-ENV:Body>\r\n"
-  "<ns1:getServerTime>\r\n"
-  "</ns1:getServerTime>\r\n"
- "</SOAP-ENV:Body>\r\n"
+"    xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\"\r\n"
+"    xmlns:SOAP-ENC=\"http://schemas.xmlsoap.org/soap/encoding/\"\r\n"
+"    xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\r\n"
+"    xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"\r\n"
+"    xmlns:ns1=\"urn:service\">\r\n"
+" <SOAP-ENV:Body>\r\n"
+"  <ns1:getRefbookList>\r\n"
+"   <userKey></userKey>\r\n"
+"  </ns1:getRefbookList>\r\n"
+" </SOAP-ENV:Body>\r\n"
 "</SOAP-ENV:Envelope>\r\n";
-	
-/*"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"*/
-/*"<SOAP-ENV:Envelope\r\n"*/
-/*"    xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\"\r\n"*/
-/*"    xmlns:SOAP-ENC=\"http://schemas.xmlsoap.org/soap/encoding/\"\r\n"*/
-/*"    xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\r\n"*/
-/*"    xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"\r\n"*/
-/*"    xmlns:ns1=\"urn:service\">\r\n"*/
-/*" <SOAP-ENV:Body>\r\n"*/
-/*"  <ns1:getRefbookList>\r\n"*/
-/*"   <userKey></userKey>\r\n"*/
-/*"  </ns1:getRefbookList>\r\n"*/
-/*" </SOAP-ENV:Body>\r\n"*/
-/*"</SOAP-ENV:Envelope>\r\n";*/
 
 	url_request_set_http_body_from_string(request, "text/xml;charset=UTF-8", msg);	
 
