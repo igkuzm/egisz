62542aa3be73428242ddd0477ec1d78ba829f605
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-19 09:38:46 +1000

    Updated dictionary.h

---
+++ dictionary.h
@@ -0,0 +1,15 @@
+/**
+ * File              : dictionary.h
+ * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
+ * Date              : 18.10.2021
+ * Last Modified Date: 19.10.2021
+ * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
+ */
+#include <stdio.h>
+
+struct Dictionary_t {
+	char key[128];
+	void *value;
+	struct Dictionary_t *next;
+};
+
