c1dbe4ec6b303a23ed0fb6dcf0c5eafaf427b175
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 17:58:53 +1000

    Updated url_connector.c

--- url_connector.c
+++ url_connector.c
@@ -93,6 +93,7 @@ void url_request_add_header_item(URLRequest *request, HTTP_HEADER_ITEM_KEY key, 
 	item->key = key;
 	strcpy(item->value, value);
 }
+
 void url_request_add_header_item(URLRequest *request, HTTP_HEADER_ITEM_KEY key){
 
 }
