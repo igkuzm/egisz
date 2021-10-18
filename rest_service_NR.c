bc44e9d3aacb502606d55e812cc71048c529e06e
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 11:13:16 +1000

    Updated rest_service_NR.c

--- rest_service_NR.c
+++ rest_service_NR.c
@@ -156,7 +156,7 @@ int rest_service_answer_callback(char *str, int len, int *count, void *_answer){
 		}
 		if (we_have_chunked_size) {
 		//remove chunked_size string from str
-			for (i = 0; i < 7; ++i) {
+			for (i = 0; i < 8; ++i) {
 				memmove(&str[start_of_chunked],&str[start_of_chunked + 1],len-1);
 				len--;
 			}
