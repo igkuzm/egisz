ef641f3a9216f878c63abb3cc9a534b84da2d432
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 17:36:42 +1000

    Updated nsi.c

--- nsi.c
+++ nsi.c
@@ -16,6 +16,7 @@ URLRequest *url_request_prepare(){
 	url_request_set_port(request, PORT);
 	url_request_set_hostname(request, HOST);
 	url_request_set_http_method(request, HTTP_METHOD_POST);
+	url_request_set_url_connection_protocol(request, URL_CONNECTION_PROTOCOL_HTTPS);
 		
 		url_request_new_with_string("https://nsi.rosminzdrav.ru/wsdl/SOAP-server.v2.php");
 	url_request_add_header_item(request, HTTP_HEADER_ITEM_KEY_Connection, "close");
