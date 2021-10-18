1e20643a650c310181c4972f0b533cb9877fe2c2
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 17:54:40 +1000

    Updated nsi.c

--- nsi.c
+++ nsi.c
@@ -65,4 +65,25 @@ ezxml_t egisz_nsi_get_server_time(){
 	return nsi_xml_from_url_connection_send_request(request); 
 }
 
+ezxml_t egisz_nsi_get_refbook_list(){
+	URLRequest *request = nsi_url_request_prepare(); 
+	char *msg = 
+	"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"
+	"<SOAP-ENV:Envelope\r\n"
+	" xmlns:SOAP-ENV=\"http:schemas.xmlsoap.org/soap/envelope/\"\r\n"
+	" xmlns:SOAP-ENC=\"http:schemas.xmlsoap.org/soap/encoding/\"\r\n"
+	" xmlns:xsi=\"http:www.w3.org/2001/XMLSchema-instance\"\r\n"
+	" xmlns:xsd=\"http:www.w3.org/2001/XMLSchema\"\r\n"
+	" xmlns:ns1=\"urn:service\">\r\n"
+	" <SOAP-ENV:Body>\r\n"
+	"  <ns1:getServerTime>\r\n"
+	"  </ns1:getServerTime>\r\n"
+	" </SOAP-ENV:Body>\r\n"
+	"</SOAP-ENV:Envelope>\r\n";
+	url_request_set_http_body_from_string(request, "text/xml;charset=UTF-8", msg);
+	url_request_add_header_item(request, HTTP_HEADER_ITEM_KEY_SOAPAction, "");
+
+	return nsi_xml_from_url_connection_send_request(request); 
+}
+
 
