91a2bfede4f26a8b34bc4d11a440e3b22aca9c79
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 13:35:39 +1000

    Updated url_connector.h

--- url_connector.h
+++ url_connector.h
@@ -1,6 +1,31 @@
 /**
  * File              : url_connector.h
  * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
+ * Date              : 18.10.2021
+ * Last Modified Date: 18.10.2021
+ * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
+ */
+/**
+ * url_connector.h
+ * Copyright (c) 2021 Igor V. Sementsov <ig.kuzm@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+/**
+ * File              : url_connector.h
+ * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
  * Date              : 15.10.2021
  * Last Modified Date: 18.10.2021
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
