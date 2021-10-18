a826b7ab6058742428f3ce8a34da4dc7a3d466ae
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 16:14:32 +1000

    Updated url_connector.c

--- url_connector.c
+++ url_connector.c
@@ -363,9 +363,9 @@ int url_connection_send_request_ssl(int sd, char *write_buf, void *data, int (*c
 			}
 			count++; //we need count to know how many times callback was called
 		}
-		else {
+		//else {
 			printf("%s", buf); //print for debug
-		}
+		//}
 	}
 	if (bytes < 0 ){ //hendle with error
 		_handle_with_ssl_error(ssl, bytes);
