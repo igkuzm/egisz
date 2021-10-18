1db11e24c56d2f7be6cf5bec67d87140898ef08d
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 19:28:51 +1000

    Updated rest_service_NR.h

--- rest_service_NR.h
+++ rest_service_NR.h
@@ -16,6 +16,7 @@ struct	egisz_rest_refs_list_struct {
 };
 
 int egisz_rest_refs_list(struct egisz_rest_refs_list_struct *array); //Получение списка справочников
+//возвращает количество записей массива array
 
 cJSON *egisz_rest_refs_parts //Возвращает количество частей справочника, которое можно получить
 ( 
