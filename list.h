624e967de2bcbb31e6d7b453d8ef9967ad674dcf
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 18:26:59 +1000

    Updated list.h

--- list.h
+++ list.h
@@ -6,3 +6,11 @@
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
  */
 #include <stdio.h>
+
+struct LIST_t {
+   int data;
+   int key;
+   struct LIST_t *next;
+};
+
+typedef LIST_t LIST;
