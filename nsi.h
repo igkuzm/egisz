f0b5ba7ade8e7f0b770490d475abdbf7e6d4b5dc
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 17:29:57 +1000

    Updated nsi.h

---
+++ nsi.h
@@ -0,0 +1,8 @@
+/**
+ * File              : nsi.h
+ * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
+ * Date              : 18.10.2021
+ * Last Modified Date: 18.10.2021
+ * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
+ */
+#include <stdio.h>
