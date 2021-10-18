d4685efd2d3ebc608233ae74322a6e519c26fa6a
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-19 09:45:19 +1000

    Updated dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -26,6 +26,7 @@ void dictionary_print_data(Dictionary *dictionary) {
 
 Dictionary *dictionary_add_value_for_key(Dictionary *head, void *value, const char key){
    Dictionary *ptr = head;	
-
-
+   while(ptr != NULL) {
+      ptr = ptr->next;
+   }
 }
