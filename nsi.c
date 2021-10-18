b3ba756796337cab4d948e04d8bac722061762d3
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 17:38:56 +1000

    Updated nsi.c

--- nsi.c
+++ nsi.c
@@ -41,8 +41,7 @@ ezxml_t egisz_nsi_get_server_time(){
 	"  </ns1:getServerTime>\r\n"
 	" </SOAP-ENV:Body>\r\n"
 	"</SOAP-ENV:Envelope>\r\n";
-	
-
+	url_request_set_http_body_from_string(request, const char *contentType, msg);
 }
 
 
