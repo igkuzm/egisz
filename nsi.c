ac9f583d767e3839a9496662081395677960c606
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-18 21:28:20 +1000

    Updated nsi.c

--- nsi.c
+++ nsi.c
@@ -47,18 +47,18 @@ ezxml_t nsi_xml_from_url_connection_send_request(URLRequest *request){
 
 ezxml_t egisz_nsi_get_server_time(){
 	URLRequest *request = nsi_url_request_prepare(); 
-	char *msg = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-	"<SOAP-ENV:Envelope\n"
-	"	xmlns:SOAP-ENV=\"http:schemas.xmlsoap.org/soap/envelope/\"\n"
-	"	xmlns:SOAP-ENC=\"http:schemas.xmlsoap.org/soap/encoding/\"\n"
-	"	xmlns:xsi=\"http:www.w3.org/2001/XMLSchema-instance\"\n"
-	"	xmlns:xsd=\"http:www.w3.org/2001/XMLSchema\"\n"
-	"	xmlns:ns1=\"urn:service\">\n"
-	"	<SOAP-ENV:Body>\n"
-	"		<ns1:getServerTime>\n"
-	"		</ns1:getServerTime>\n"
-	"	</SOAP-ENV:Body>\n"
-	"</SOAP-ENV:Envelope>\n";
+	char *msg = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n"
+	"<SOAP-ENV:Envelope\r\n"
+	"	xmlns:SOAP-ENV=\"http:schemas.xmlsoap.org/soap/envelope/\"\r\n"
+	"	xmlns:SOAP-ENC=\"http:schemas.xmlsoap.org/soap/encoding/\"\r\n"
+	"	xmlns:xsi=\"http:www.w3.org/2001/XMLSchema-instance\"\r\n"
+	"	xmlns:xsd=\"http:www.w3.org/2001/XMLSchema\"\r\n"
+	"	xmlns:ns1=\"urn:service\">\r\n"
+	"	<SOAP-ENV:Body>\r\n"
+	"		<ns1:getServerTime>\r\n"
+	"		</ns1:getServerTime>\r\n"
+	"	</SOAP-ENV:Body>\r\n"
+	"</SOAP-ENV:Envelope>\r\n";
 	url_request_set_http_body_from_string(request, "text/xml;charset=UTF-8", msg);
 
 	return nsi_xml_from_url_connection_send_request(request); 
