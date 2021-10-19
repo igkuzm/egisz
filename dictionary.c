88acea43e01fe13b8bdf04adbb1be03a1cfb1d4b
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-19 10:05:05 +1000

    Updated dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -25,8 +25,12 @@ void dictionary_print_data(Dictionary *dictionary) {
 }
 
 Dictionary *dictionary_new(){
-
-
+   Dictionary *dictionary = malloc(sizeof(Dictionary));
+   if (dictionary == NULL) { //check memory allocation
+		fprintf(stderr, "ERROR to allocate memory for dictionary\n");
+		exit(ENOMEM);
+   }
+   return dictionary;
 }
 
 void dictionary_add_value_for_key(Dictionary *dictionary, void *value, const char *key){
@@ -41,11 +45,6 @@ void dictionary_add_value_for_key(Dictionary *dictionary, void *value, const cha
 	}
 }
 Dictionary *dictionary_new_with_value_for_key(void *value, const char *key){
-   Dictionary *dictionary = malloc(sizeof(Dictionary));
-   if (dictionary == NULL) { //check memory allocation
-		fprintf(stderr, "ERROR to allocate memory for dictionary\n");
-		exit(ENOMEM);
-   }
    dictionary->next = NULL;
    strncpy(dictionary->key, key, 127);
    dictionary->key[127] = 0;
