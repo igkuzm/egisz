2c5539d2b4d35ad8a390f781b457a2c9602618cc
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-19 10:07:49 +1000

    Updated dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -42,6 +42,7 @@ void dictionary_set_value_for_key(Dictionary *dictionary, void *value, const cha
 
 Dictionary *dictionary_new_with_value_for_key(void *value, const char *key){
 	Dictionary *dictionary = dictionary_new();
+	dictionary_set_value_for_key(dictionary, value, key);
 	return dictionary;
 }
 void dictionary_add_value_for_key(Dictionary *dictionary, void *value, const char *key){
