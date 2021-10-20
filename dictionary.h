853542b21fc0891eaa9d31f128037fe2ff8ef427
Igor Sementsov <ig.kuzm@gmail.com>
2021-10-20 21:16:16 +1000

    Updated dictionary.h

--- dictionary.h
+++ dictionary.h
@@ -2,7 +2,7 @@
  * File              : dictionary.h
  * Author            : Igor V. Sementsov <ig.kuzm@gmail.com>
  * Date              : 18.10.2021
- * Last Modified Date: 19.10.2021
+ * Last Modified Date: 20.10.2021
  * Last Modified By  : Igor V. Sementsov <ig.kuzm@gmail.com>
  */
 #include <stdio.h>
@@ -34,3 +34,9 @@ void dictionary_add_value_for_key(Dictionary *dictionary, void *value, DICTIONAR
 void dictionary_set_value_for_key(Dictionary *dictionary, void *value, DICTIONARY_DATA_TYPE data_type, const char *key);
 void *dictionary_get_value_for_key(Dictionary *dictionary, const char *key);
 void dictionary_remove_with_key(Dictionary *dictionary, const char *key);
+
+struct Array_t {
+	void *data;
+	int count;
+}
+typedef struct Array_t Array;
